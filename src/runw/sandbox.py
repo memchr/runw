0ac9c51a4d2dad779d8f4167445f9da9099da842
memchr <memchr@proton.me>
2025-07-08 18:53:11 +0000

    refactor: move launch() to Sandbox.__init__()

--- src/runw/sandbox.py
+++ src/runw/sandbox.py
@@ -7,7 +7,8 @@ from glob import glob
 from os.path import expandvars
 from pathlib import Path
 from reprlib import Repr
-from typing import Literal, NotRequired, Self, TypedDict
+from typing import Callable, Literal, NotRequired, Self, TypedDict
+
 from .constants import (
     DBUS_PROXY_PATH,
     HOME,
@@ -17,7 +18,6 @@ from .constants import (
     XDG_DATA_HOME,
     XDG_RUNTIME_DIR,
 )
-
 
 MountMode = Literal["dev", "ro", "rw"]
 
@@ -46,124 +46,12 @@ ar = Repr(indent=2, maxdict=200, maxstring=200, maxlist=200)
 
 
 def run(config: AppConfig, shell=False):
-    sandbox, cmd = create(config)
-    sandbox.exec(cmd)
-
-
-def create(config: AppConfig) -> tuple["Sandbox", list[str]]:
-    logging.debug("config:\n %s", ar.repr(config))
-    sandbox = (
-        Sandbox()
-        .share(*config.get("share", []))
-        .bind(
-            "/usr",
-            "/etc",
-            "/opt",
-            "/var/lib/alsa/",
-            "/run/systemd/resolve/",
-            "/tmp/.X11-unix",
-            "/tmp/.ICE-unix",
-        )
-        .dir(
-            "/var/empty",
-            "/tmp",
-        )
-        .link(
-            ("/usr/bin", "/bin"),
-            ("/usr/bin", "/sbin"),
-            ("/usr/lib", "/lib"),
-            ("/usr/lib64", "/lib64"),
-            ("/run", "/var/run"),
-        )
-        .options("--proc", "/proc", "--dev", "/dev")
-        .bind(
-            "/dev/dri",
-            "/dev/input",
-            "/dev/hugepages",
-            *glob("/dev/nvidia*"),
-            "/dev/snd",
-            "/dev/fuse",
-            "/sys/block/",
-            "/sys/bus/",
-            "/sys/class/",
-            "/sys/dev/",
-            "/sys/devices/",
-            "/sys/module/",
-            default_mode="dev",
-        )
-        .home(config.get("home"))
-        .bind(
-            # graphics
-            *glob(str(XDG_RUNTIME_DIR / "wayland*")),
-            # sound
-            *glob(str(XDG_RUNTIME_DIR / "pulse*")),
-            *glob(str(XDG_RUNTIME_DIR / "pipewire*")),
-            # cache
-            XDG_CACHE_HOME / "mesa_shader_cache",
-            XDG_CACHE_HOME / "radv_builtin_shaders64",
-            XDG_CACHE_HOME / "nv",
-            XDG_CACHE_HOME / "nvidia",
-            XDG_CACHE_HOME / "radv_builtin_shaders",
-            XDG_CACHE_HOME / "mesa_shader_cache_db",
-            # other
-            XDG_CONFIG_HOME / "MangoHud",
-            {"src": XDG_CONFIG_HOME / "user-dirs.dirs", "mode": "ro"},
-            {"src": XDG_CONFIG_HOME / "user-dirs.locale", "mode": "ro"},
-        )
-    )
-
-    if config["kill"]:
-        logging.debug("Ensures child process dies when bwrap exits")
-        sandbox.options("--die-with-parent")
-
-    if config["nvidia"]:
-        logging.debug("force nvidia gpu")
-        sandbox.setenv(
-            __NV_PRIME_RENDER_OFFLOAD="1",
-            __GLX_VENDOR_LIBRARY_NAME="nvidia",
-            __VK_LAYER_NV_optimus="NVIDIA_only",
-            VK_DRIVER_FILES="/usr/share/vulkan/icd.d/nvidia_icd.json",
-        )
-
-    if "bus" in config or "system_bus" in config:
-        sandbox.dbus_proxy(config.get("bus", []), config.get("system_bus", []))
-
-    # setup launchers and command
-    cmd: list[str] = []
-    for launcher in config.get("launcher", []):
-        match launcher:
-            case "mangohud":
-                cmd.append("mangohud")
-            case "proton":
-                compat_data = XDG_DATA_HOME / "proton"
-                compat_data.mkdir(exist_ok=True, parents=True)
-
-                logging.debug(
-                    "use proton\n"
-                    + f"  path: {PROTON_PATH.parent}\n"
-                    + f"  prefix: {compat_data}"
-                )
-                sandbox.setenv(
-                    STEAM_COMPAT_CLIENT_INSTALL_PATH=str(XDG_DATA_HOME / "Steam"),
-                    STEAM_COMPAT_DATA_PATH=str(compat_data),
-                ).bind(compat_data, PROTON_PATH.parent)
-                cmd.extend([str(PROTON_PATH), "run"])
-            case "wine":
-                logging.debug("use wine")
-                sandbox.bind(HOME / ".wine")
-                cmd.append("wine")
-            case _:
-                pass
-    cmd.extend(expandvars(c) for c in config["cmd"])
-    logging.debug("command: %s", cmd)
-
-    sandbox.bind(*config["binds"]).setenv(**config.get("env", {}))
-
+    sandbox = Sandbox(config)
     if shell:
         logging.debug("runing shell")
         sandbox.exec([os.getenv("SHELL", "bash")])
     else:
-        sandbox.exec(cmd)
+        sandbox.exec()
 
 
 def openfd(content: bytes) -> int:
@@ -174,14 +62,130 @@ def openfd(content: bytes) -> int:
 
 
 class Sandbox:
-    def __init__(self) -> None:
+    def __init__(self, config: AppConfig | None = None) -> None:
         self._options: list[str] = []
+        self._hooks: list[Callable[[], None]] = []
+        if config is None:
+            return
+        logging.debug("config:\n %s", ar.repr(config))
+        (
+            self.share(*config.get("share", []))
+            .bind(
+                "/usr",
+                "/etc",
+                "/opt",
+                "/var/lib/alsa/",
+                "/run/systemd/resolve/",
+                "/tmp/.X11-unix",
+                "/tmp/.ICE-unix",
+            )
+            .dir(
+                "/var/empty",
+                "/tmp",
+            )
+            .link(
+                ("/usr/bin", "/bin"),
+                ("/usr/bin", "/sbin"),
+                ("/usr/lib", "/lib"),
+                ("/usr/lib64", "/lib64"),
+                ("/run", "/var/run"),
+            )
+            .options("--proc", "/proc", "--dev", "/dev")
+            .bind(
+                "/dev/dri",
+                "/dev/input",
+                "/dev/hugepages",
+                *glob("/dev/nvidia*"),
+                "/dev/snd",
+                "/dev/fuse",
+                "/sys/block/",
+                "/sys/bus/",
+                "/sys/class/",
+                "/sys/dev/",
+                "/sys/devices/",
+                "/sys/module/",
+                default_mode="dev",
+            )
+            .home(config.get("home"))
+            .bind(
+                # graphics
+                *glob(str(XDG_RUNTIME_DIR / "wayland*")),
+                # sound
+                *glob(str(XDG_RUNTIME_DIR / "pulse*")),
+                *glob(str(XDG_RUNTIME_DIR / "pipewire*")),
+                # cache
+                XDG_CACHE_HOME / "mesa_shader_cache",
+                XDG_CACHE_HOME / "radv_builtin_shaders64",
+                XDG_CACHE_HOME / "nv",
+                XDG_CACHE_HOME / "nvidia",
+                XDG_CACHE_HOME / "radv_builtin_shaders",
+                XDG_CACHE_HOME / "mesa_shader_cache_db",
+                # other
+                XDG_CONFIG_HOME / "MangoHud",
+                {"src": XDG_CONFIG_HOME / "user-dirs.dirs", "mode": "ro"},
+                {"src": XDG_CONFIG_HOME / "user-dirs.locale", "mode": "ro"},
+            )
+        )
 
-    def exec(self, cmd: list[str]):
+        if config["kill"]:
+            logging.debug("Ensures child process dies when bwrap exits")
+            self.options("--die-with-parent")
+
+        if config["nvidia"]:
+            logging.debug("force nvidia gpu")
+            self.setenv(
+                __NV_PRIME_RENDER_OFFLOAD="1",
+                __GLX_VENDOR_LIBRARY_NAME="nvidia",
+                __VK_LAYER_NV_optimus="NVIDIA_only",
+                VK_DRIVER_FILES="/usr/share/vulkan/icd.d/nvidia_icd.json",
+            )
+
+        if "bus" in config or "system_bus" in config:
+            self.dbus_proxy(config.get("bus", []), config.get("system_bus", []))
+
+        # setup launchers and command
+        self.cmd: list[str] = []
+        for launcher in config.get("launcher", []):
+            match launcher:
+                case "mangohud":
+                    self.cmd.append("mangohud")
+                case "proton":
+                    compat_data = XDG_DATA_HOME / "proton"
+                    compat_data.mkdir(exist_ok=True, parents=True)
+
+                    logging.debug(
+                        "use proton\n"
+                        + f"  path: {PROTON_PATH.parent}\n"
+                        + f"  prefix: {compat_data}"
+                    )
+                    self.setenv(
+                        STEAM_COMPAT_CLIENT_INSTALL_PATH=str(XDG_DATA_HOME / "Steam"),
+                        STEAM_COMPAT_DATA_PATH=str(compat_data),
+                    ).bind(compat_data, PROTON_PATH.parent)
+                    self.cmd.extend([str(PROTON_PATH), "run"])
+                case "wine":
+                    logging.debug("use wine")
+                    self.bind(HOME / ".wine")
+                    self.cmd.append("wine")
+                case _:
+                    pass
+        self.cmd.extend(expandvars(c) for c in config["cmd"])
+        logging.debug("command: %s", self.cmd)
+
+        self.bind(*config["binds"]).setenv(**config.get("env", {}))
+
+    def exec(self, cmd: list[str] | None = None):
+        for hook in self._hooks:
+            hook()
         logging.debug("bwrap args %s", ar.repr(self._options))
         os.execvp(
             "bwrap",
-            ["bwrap", "--args", str(openfd("\0".join(self._options).encode())), *cmd],
+            [
+                "bwrap",
+                "--args",
+                str(openfd("\0".join(self._options).encode())),
+                *(cmd or self.cmd),
+            ],
         )
 
     _BIND_VERBS = {"ro": "--ro-bind-try", "rw": "--bind-try", "dev": "--dev-bind-try"}
@@ -251,29 +255,30 @@ class Sandbox:
         )
 
         fd_bwrap, fd_dbus_proxy = os.pipe()
-        subprocess.Popen(
-            [
-                "/usr/bin/xdg-dbus-proxy",
-                f"--fd={fd_dbus_proxy}",
-                # session bus
-                os.getenv("DBUS_SESSION_BUS_ADDRESS", f"unix:path={session_bus}"),
-                str(session_bus_proxy),
-                "--filter",
-                *session_bus_filter,
-                # system bus
-                f"unix:path={system_bus}",
-                str(system_bus_proxy),
-                "--filter",
-                *system_bus_filter,
-            ],
-            pass_fds=[fd_dbus_proxy],
-        )
 
-        os.read(fd_bwrap, 1)
-        logging.debug("xdg-dbus-proxy is ready")
-
-        os.set_inheritable(fd_bwrap, True)
         self.bind(
             {"src": session_bus_proxy, "dest": session_bus},
             {"src": system_bus_proxy, "dest": system_bus},
         ).options("--sync-fd", str(fd_bwrap))
+        args = [
+            "/usr/bin/xdg-dbus-proxy",
+            f"--fd={fd_dbus_proxy}",
+            # session bus
+            os.getenv("DBUS_SESSION_BUS_ADDRESS", f"unix:path={session_bus}"),
+            str(session_bus_proxy),
+            "--filter",
+            *session_bus_filter,
+            # system bus
+            f"unix:path={system_bus}",
+            str(system_bus_proxy),
+            "--filter",
+            *system_bus_filter,
+        ]
+
+        def run():
+            subprocess.Popen(args, pass_fds=[fd_dbus_proxy])
+            os.read(fd_bwrap, 1)
+            logging.debug("xdg-dbus-proxy is ready")
+            os.set_inheritable(fd_bwrap, True)
+
+        self._hooks.append(run)
