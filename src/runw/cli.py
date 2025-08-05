683f92428b4b1b7eaaf06978dc30ea9f743f200b
memchr <memchr@proton.me>
2025-08-05 05:39:25 +0000

    feat: support starting profiles via symlinks

--- src/runw/cli.py
+++ src/runw/cli.py
@@ -1,39 +1,65 @@
 import argparse
 import logging
 import os
+import sys
 
 from runw.config import load_configs, load_presets
 from runw.sandbox import Bwrap
 # pyright: reportUninitializedInstanceVariable=false
 
 
-class Arguments(argparse.Namespace):
-    verbose: bool
-    shell: bool
-    cmd: list[str] | None
-    container: str | None
-    args: list[str]
-    list: bool
+class RunArgumentParser(argparse.ArgumentParser):
+    class Arguments(argparse.Namespace):
+        verbose: bool
+        shell: bool
+        cmd: list[str] | None
+        container: str | None
+        args: list[str]
+        list: bool
+
+    def __init__(self):
+        super().__init__(
+            description="Run preconfigured bubblewrap containers",
+            allow_abbrev=False,
+            usage="runw [options] container [args]\n       runw -l",
+        )
+        self.add_argument(
+            "-s", "--shell", action="store_true", help="Run a temporary shell"
+        )
+        self.add_argument(
+            "-v", "--verbose", action="store_true", help="Enable debug logging"
+        )
+        self.add_argument("-c", "--cmd", type=str, help="override app command", nargs=1)
+        self.add_argument("-l", "--list", action="store_true", help="List all apps")
+        self.add_argument("container", nargs="?")
+        self.add_argument("args", nargs=argparse.REMAINDER, help="additional arguments")
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Run preconfigured bubblewrap containers",
-        allow_abbrev=False,
-        usage="runw [options] container [args]\n       runw -l",
-    )
-    parser.add_argument(
-        "-s", "--shell", action="store_true", help="Run a temporary shell"
-    )
-    parser.add_argument(
-        "-v", "--verbose", action="store_true", help="Enable debug logging"
-    )
-    parser.add_argument("-c", "--cmd", type=str, help="override app command", nargs=1)
-    parser.add_argument("-l", "--list", action="store_true", help="List all apps")
-    parser.add_argument("container", nargs="?")
-    parser.add_argument("args", nargs=argparse.REMAINDER, help="additional arguments")
+    progname = os.path.basename(sys.argv[0])
+    if progname == "runw":
+        runw()
+    else:
+        run_config(progname)
 
-    args = parser.parse_args(namespace=Arguments())
+
+def run_config(profile_name: str):
+    presets = load_presets()
+    configs = load_configs()
+    try:
+        sandbox = Bwrap.from_dict(configs[profile_name])
+    except KeyError:
+        print(f"{profile_name} not found")
+        raise SystemExit
+    sandbox = sandbox.resolve(presets)
+    sandbox.cmd.extend(sys.argv[1:])
+    sandbox.exec()
+
+
+def runw():
+    parser = RunArgumentParser()
+
+    args = parser.parse_args(namespace=RunArgumentParser.Arguments())
 
     if args.verbose:
         log_level = logging.DEBUG
